cb50d44720cfbf8de395ab8e5f466eb1eb3b845d
santomon <qa12_8@yahoo.de>
2023-05-27 17:39:06 +0200

    will be renaming example.ts soon; now integrating with langchain for nwo

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,12 +1,34 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+import { Configuration, OpenAIApi } from "openai";
+import {OpenAI} from "langchain/llms/openai"
+import {PromptTemplate} from "langchain";
+import ignore from "ignore";
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+const model = new OpenAI({ temperature: 0.9,
+    modelName: "gpt-3.5-turbo",
+
+})
+const prompt = new PromptTemplate({
+    template: "This is a {test} prompt",
+    inputVariables: ["test"],
+})
+
+
+
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query( async ({ input }) => {
+        const result = model.call(await prompt.format({test: input.text}))
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `Hello ${input.text}`
       };
     }),
   getAll: publicProcedure.query(({ ctx }) => {

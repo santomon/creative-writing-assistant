90ce548b4469c0766dff2f234677c0cf9a92f34e
santomon <qa12_8@yahoo.de>
2023-05-27 17:58:51 +0200

    Revert "committing everything for now"
    This reverts commit 1533bd16296d5a1a4450017598a7f8ff0e4b3c8d.

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -4,6 +4,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { Configuration, OpenAIApi } from "openai";
 import {OpenAI} from "langchain/llms/openai"
 import {PromptTemplate} from "langchain";
+import ignore from "ignore";
 
 // const configuration = new Configuration({
 //     apiKey: process.env.OPENAI_API_KEY,
@@ -14,64 +15,9 @@ const model = new OpenAI({ temperature: 0.9,
     modelName: "gpt-3.5-turbo",
 
 })
-const eventTemplate = new PromptTemplate({
-    template: `   
-Prompt:
-
-Please extract the events from my creative text about the world and the story i am currently working on as a list
-
-the output should look like so (JSON Object model)
-
-Output format:
-[
-  \{
-    "id": "string",
-    "name": "string",
-    "description": "string",
-    "begin": float,
-    "end": float,
-    "timeframe": "string"
-    "significance": int  // value between 1 and 100
-  \}
-]
-
-Example JSON output:
-json
-[
-  \{
-    "id": "event_1",
-    "name": "The Great War",
-    "description": "A devastating war breaks out between two powerful nations, resulting in widespread destruction and loss of life.",
-    "begin": 14353,
-    "end": 14355,
-    "timeframe": "20th century",
-    "significance": 90
-  \},
-  \{
-    "id": "event_2",
-    "name": "Discovery of a New Continent",
-    "description": "Explorers stumble upon an uncharted continent, rich in resources and inhabited by unknown civilizations.",
-    "begin": 1587,
-    "end": 1590,
-    "timeframe": "16th century",
-    "significance": 100,
-  \}
-]
-
-further explanations:
-- the time_begin and time_end are arbitrary integer; the only rule here is that if a number is lower than the other, that
-means it happened before.
-
-- for the significance, rate the impact of the event with respect to the world. events like large scale wars are considered
-significant, characters travelling might be considered less significant.
-
-- the event_id can be left undefined
-
-the text is given in the following inside "\`\`\`"
-
-    \`\`\`{text}\`\`\`
-    `,
-    inputVariables: ["text"],
+const prompt = new PromptTemplate({
+    template: "This is a {test} prompt",
+    inputVariables: ["test"],
 })
 
 
@@ -79,8 +25,8 @@ the text is given in the following inside "\`\`\`"
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(  ({ input }) => {
-        // const result = await model.call(await eventTemplate.format({text: input.text}))
+    .query( async ({ input }) => {
+        const result = model.call(await prompt.format({test: input.text}))
       return {
         greeting: `Hello ${input.text}`
       };

8d93376dee6c7322c304730038472b88e7b7444d
santomon <qa12_8@yahoo.de>
2023-05-29 17:52:05 +0200

    Revert "removed traces of trpc"
    This reverts commit bd5c3102657fb25142876cf32e8d9ada137edce9.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { api } from "~/utils/api";
 import {useUser} from "@clerk/nextjs";
 
 import React, {useState} from 'react';

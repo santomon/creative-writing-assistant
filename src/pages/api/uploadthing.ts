37e60ed72ba6d77a34e0b315a5f3257d369ee22e
santomon <qa12_8@yahoo.de>
2023-05-31 19:05:39 +0200

    added the pages endpoint or sth

---
+++ src/pages/api/uploadthing.ts
@@ -0,0 +1,10 @@
+/** pages/api/uploadthing.ts */
+import { createNextPageApiHandler } from "uploadthing/next-legacy";
+
+import { ourFileRouter } from "~/server/api/routers/uploadthing";
+
+const handler = createNextPageApiHandler({
+  router: ourFileRouter,
+});
+
+export default handler;
